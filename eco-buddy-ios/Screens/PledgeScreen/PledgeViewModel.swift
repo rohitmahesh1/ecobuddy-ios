3736cd10c46688f8e8bd11ecceb4088db867078b
Rohit Mahesh <imrohitmahesh1@gmail.com>
2026-03-21 03:06:23 -0400

    fixed pledge list loading

--- eco-buddy-ios/Screens/PledgeScreen/PledgeViewModel.swift
+++ eco-buddy-ios/Screens/PledgeScreen/PledgeViewModel.swift
@@ -10,7 +10,7 @@ import Foundation
 class PledgeViewModel: ObservableObject {
     
     struct ChallengeVM: Identifiable {
-        let id = UUID().uuidString
+        let id: String
         var challenge: Challenge
         var onTap: (() -> Void)?
         var isSubCompleted: Bool
@@ -28,17 +28,23 @@ class PledgeViewModel: ObservableObject {
     }
     
     private func getChallenges() {
-        guard let category = self.persistentStorage.getCategories().filter({ $0.categoryId == "2" }).first else { return }
+        guard let category = self.persistentStorage.getCategories().filter({ $0.categoryId == "2" }).first else {
+            self.challengesVM = []
+            self.selectedChallenge = nil
+            return
+        }
         
        
         let challenges = persistentStorage.getChallenges()
         let filteredChallenges = challenges.filter({ challenge in
             category.challengeIds.contains(challenge.wrappedChallengeId)
-        }).prefix(5)
+        })
         
+        self.selectedChallenge = nil
         challengesVM = filteredChallenges.map({ challenge in
             
             return ChallengeVM(
+                id: challenge.wrappedChallengeId,
                 challenge: challenge,
                 onTap: {
                     self.selectedChallenge = challenge
